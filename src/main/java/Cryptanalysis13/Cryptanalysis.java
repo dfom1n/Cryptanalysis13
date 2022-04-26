95015844761a2825563ced4d337255dc9a11f632
Fomin Dmitriy <dm.a.fom1n@gmail.com>
2022-04-26 23:04:30 +0300

    First commit

---
+++ src/main/java/Cryptanalysis13/Cryptanalysis.java
@@ -0,0 +1,10 @@
+package Cryptanalysis13;
+
+public class Cryptanalysis {
+    public static void main(String[] args) {
+        System.out.println(13);
+        System.out.println(13);
+        System.out.println(13);
+        System.out.println(13);
+    }
+}
